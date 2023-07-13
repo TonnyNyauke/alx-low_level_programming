244ab8e3363c7fc144ab0f146120f5714695bebf
TonnNyauke <nyaukeindustries@gmail.com>
2023-07-13 04:51:55 -0700

    Changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+
 /**
- * Entry point of execution
+ * main -  prints exactly "Programming is like building a multilingual puzzle,
+ * followed by a new line.
+ * return 0.
  */
 int main(void)
 {
