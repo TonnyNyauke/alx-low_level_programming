a183924859a46a922918abc6de8dac98ab46252f
TonnNyauke <nyaukeindustries@gmail.com>
2023-07-18 19:15:42 -0700

    Changes

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,27 +5,28 @@
  */
 void times_table(void)
 {
-	int i, j, p;
+	int x, y, z, u, d;
 
-	for (i = 0; i < 10; i++)
+	for (x = 0; x <= 9; x++)
 	{
-		for (j = 0; j < 10; j++)
+		for (y = 0; y <=9; y++)
 		{
-			p = i * j;
-			if (p < 10)
+			z = x * y;
+			if (z > 9)
 			{
-				_putchar(' ');
-				_putchar('0' + p);
+				u = z % 10;
+				d = (z - u) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
 			}
-			else
+			else if (y != 0)
 			{
-				_putchar('0' + (p / 10));
-				_putchar('0' + (p % 10));
-			}
-			if (j < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+				_putchar(z + '0');
 			}
 		}
 		_putchar('\n');
