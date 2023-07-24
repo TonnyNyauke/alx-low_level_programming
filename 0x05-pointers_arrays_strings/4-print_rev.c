1da719049210554cc3faa677fa860e6c1bf9d160
TonnNyauke <nyaukeindustries@gmail.com>
2023-07-24 05:23:42 -0700

    Print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @str: pointer to the string
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int size = 0;
+	while (s[size] != '\0')
+	{
+		size ++;
+	}
+	while (size > 0)
+	{
+		_putchar(s[size -1]);
+		size--;
+	}
+	_putchar('\n');
+}
