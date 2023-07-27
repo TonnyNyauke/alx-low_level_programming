30cd011966764fc1f50238e89778a8381419f6eb
TonnNyauke <nyaukeindustries@gmail.com>
2023-07-26 18:55:10 -0700

    Changes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,20 +8,25 @@
  */
 char *cap_string(char *s)
 {
-	int i;
-	char prev_char;
+    int i;
+    char prev_char;
 
-	for (i = 0, prev_char = s[0]; s[i] != '\0'; i++)
-	{
-	if ((prev_char == ' ' || prev_char == '\t' || prev_char == '\n' ||
-		prev_char == ',' || prev_char == ';' || prev_char == '.' ||
-		prev_char == '!' || prev_char == '?' || prev_char == '"' ||
-		prev_char == '(' || prev_char == ')' || prev_char == '{' ||
-		prev_char == '}') && (s[i] >= 'a' && s[i] <= 'z'))
-	{
-		s[i] = s[i] - 'a' + 'A';
-	}
-	prev_char = s[i];
-	}
-	return (s);
+    for (i = 0, prev_char = s[0]; s[i] != '\0'; i++)
+    {
+        if ((i == 0 || prev_char == ' ' || prev_char == '\t' || prev_char == '\n' ||
+            prev_char == ',' || prev_char == ';' || prev_char == '.' ||
+            prev_char == '!' || prev_char == '?' || prev_char == '"' ||
+            prev_char == '(' || prev_char == ')' || prev_char == '{' ||
+            prev_char == '}') && (s[i] >= 'a' && s[i] <= 'z'))
+        {
+            s[i] = s[i] - 'a' + 'A';
+        }
+        else if (prev_char >= '0' && prev_char <= '9' && s[i] >= 'a' && s[i] <= 'z')
+        {
+            s[i] = s[i] - 'a' + 'A';
+        }
+        prev_char = s[i];
+    }
+    return (s);
 }
+
