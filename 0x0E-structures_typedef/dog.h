3edcfdcfa528b373bf6105ededd53b64c0bd8904
TonnNyauke <nyaukeindustries@gmail.com>
2023-08-14 05:06:59 -0700

    Work

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - a dog structure
+ * @owner: the owner of the dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ */	
+struct dog{
+	char *owner;
+	char *name;
+	float age;
+};
+
