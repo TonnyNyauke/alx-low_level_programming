e16811502e71d30604daada26829463417323769
TonnNyauke <nyaukeindustries@gmail.com>
2023-08-16 12:27:41 -0700

    Changes

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -9,4 +9,6 @@ void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char *c);
+void print_number(int n);
+void print_hex(unsigned int n);
 #endif
