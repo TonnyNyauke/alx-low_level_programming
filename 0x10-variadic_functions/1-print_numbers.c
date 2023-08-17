5fbe9d0ce5ec91c36bcd119096210fd31067de23
TonnNyauke <nyaukeindustries@gmail.com>
2023-08-17 03:16:11 -0700

    Work

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,17 +9,17 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list numbers;
-    unsigned int i;
+	va_list numbers;
+	unsigned int i;
 
-    va_start(numbers, n);
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", va_arg(numbers, int));
-        if (separator != NULL && i < n - 1)
-            printf("%s", separator);
-    }
-    printf("\n");
-    va_end(numbers);
+	va_start(numbers, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(numbers, int));
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(numbers);
 }
 
