ea84d31eddd6bd0ad1d5b30fc4cdb9d929c9cacc
TonnyNyauke <nyaukeindustries@gmail.com>
2023-08-31 18:42:47 -0700

    Work

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,32 +8,21 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
-	int h = 0;
+	int index = 0;
+	unsigned int sum = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[h] == '0' || b[h] == '1')
+	while (b[index] != '\0')
 	{
-		result <<= 1;
-		result += b[h] - '0';
-		h++;
-
-		if (*b == '0' || *b == '1')
-		{
-			result <<= 1;
-			result += *b - '0';
-		}
-		else
-		{
+		if (b[index] != '0' && b[index] != '1')
 			return (0);
-		}
+
+		sum = (sum << 1) + (b[index] - '0');
+		index++;
 	}
 
-	if (b[h] != '\0')
-		return (0);
-
-	return (result);
+	return (sum);
 }
 
