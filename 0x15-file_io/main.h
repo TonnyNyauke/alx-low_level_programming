5454679bea410872a831d2a8c999eb5347709c6b
TonnyNyauke <nyaukeindustries@gmail.com>
2023-09-04 19:22:54 -0700

    Work

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,11 +12,17 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void check_arguments(int argc, char *argv[]);
-void open_source_file(const char *filename, mode_t permissions, int *source_fd);
-void open_dest_file(const char *filename, mode_t permissions, int *dest_fd);
-void copy_file_content(int source_fd, int dest_fd);
-void close_files(int source_fd, int dest_fd);
+
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void close_elf(int elf);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 
 #define BUFSIZE 1024
 
