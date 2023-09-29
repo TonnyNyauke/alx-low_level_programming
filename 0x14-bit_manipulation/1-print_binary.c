da4d00a39b9ac31e019ee63e8fe98ed10faaceab
TonnyNyauke <nyaukeindustries@gmail.com>
2023-09-28 18:59:20 -0700

    Work

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,19 +7,19 @@
 void print_binary(unsigned long int n)
 {
 	int shift = sizeof(unsigned long int) * 8 - 1;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
+	int leading_zeros = 0;
 
 	while (shift >= 0)
 	{
-		if ((n >> shift) & 1)
-			_putchar('1');
-		else
+		if ((n >> shift) & 1 || shift == 0)
+		{
+			_putchar('0' + ((n >> shift) & 1));
+			leading_zeros = 1;
+		}
+		else if (leading_zeros)
+		{
 			_putchar('0');
+		}
 		shift--;
 	}
 }
