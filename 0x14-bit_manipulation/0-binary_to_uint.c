58be49bd4938dd87c5809c91b69c62bbcac72059
TonnyNyauke <nyaukeindustries@gmail.com>
2023-09-28 18:53:25 -0700

    Work

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,7 +5,7 @@
  * @b: A pointer to a string containing 0s and 1s.
  *
  * Return: The converted number, or 0 if there are invalid characters
- *         or if b is NULL.
+ * or if b is NULL.
  */
 unsigned int binary_to_uint(const char *b)
 {
